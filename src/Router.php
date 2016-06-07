64441c11d2f5330f4be972ad7b5f37d44044f19f
Konstantins Zukovs <konstantins@majaslapa.lv>
2016-06-07 12:27:10 +0300

    Adding some boilerplates

---
+++ src/Router.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types = 1);
+
+namespace Venta\Routing;
+
+use FastRoute\Dispatcher\GroupCountBased;
+
+/**
+ * Class Router
+ *
+ * @package Venta\Routing
+ */
+class Router extends GroupCountBased
+{
+    
+}
